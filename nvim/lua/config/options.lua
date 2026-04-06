0e83e17acf9ad8e3de7223dcd66088e7f406e841
hsrvms <59848940+hsrvms@users.noreply.github.com>
2026-04-06 07:12:35 +0300

    add ssh clipboard capability

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -3,3 +3,18 @@
 -- Add any additional options here
 
 vim.opt.winbar = "%=%m %f"
+
+-- Fallback to OSC 52 clipboard when connected via SSH
+if vim.env.SSH_TTY then
+  vim.g.clipboard = {
+    name = "OSC 52",
+    copy = {
+      ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+      ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+    },
+    paste = {
+      ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
+      ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
+    },
+  }
+end
